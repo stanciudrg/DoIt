d4fb8a3ebaf48de8948e241f8c165a508f54e473
stanciudrg <stanciudragos1337@gmail.com>
2023-11-28 10:37:28 +0200

    Install and configure html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -8,4 +9,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ]
 };
+
